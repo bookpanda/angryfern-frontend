33ceb97c6045fe8fba496636384897783cb058f5
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-15 12:17:33 +0700

    feat: country codes

--- src/components/Scoreboard/types.ts
+++ src/components/Scoreboard/types.ts
@@ -1,1 +1,505 @@
-export interface ScoreboardState {}
+export interface ScoreboardState {
+  BD: number;
+  BE: number;
+  BF: number;
+  BG: number;
+  BA: number;
+  BB: number;
+  WF: number;
+  BL: number;
+  BM: number;
+  BN: number;
+  BO: number;
+  BH: number;
+  BI: number;
+  BJ: number;
+  BT: number;
+  JM: number;
+  BV: number;
+  BW: number;
+  WS: number;
+  BQ: number;
+  BR: number;
+  BS: number;
+  JE: number;
+  BY: number;
+  BZ: number;
+  RU: number;
+  RW: number;
+  RS: number;
+  TL: number;
+  RE: number;
+  TM: number;
+  TJ: number;
+  RO: number;
+  TK: number;
+  GW: number;
+  GU: number;
+  GT: number;
+  GS: number;
+  GR: number;
+  GQ: number;
+  GP: number;
+  JP: number;
+  GY: number;
+  GG: number;
+  GF: number;
+  GE: number;
+  GD: number;
+  GB: number;
+  GA: number;
+  SV: number;
+  GN: number;
+  GM: number;
+  GL: number;
+  GI: number;
+  GH: number;
+  OM: number;
+  TN: number;
+  JO: number;
+  HR: number;
+  HT: number;
+  HU: number;
+  HK: number;
+  HN: number;
+  HM: number;
+  VE: number;
+  PR: number;
+  PS: number;
+  PW: number;
+  PT: number;
+  SJ: number;
+  PY: number;
+  IQ: number;
+  PA: number;
+  PF: number;
+  PG: number;
+  PE: number;
+  PK: number;
+  PH: number;
+  PN: number;
+  PL: number;
+  PM: number;
+  ZM: number;
+  EH: number;
+  EE: number;
+  EG: number;
+  ZA: number;
+  EC: number;
+  IT: number;
+  VN: number;
+  SB: number;
+  ET: number;
+  SO: number;
+  ZW: number;
+  SA: number;
+  ES: number;
+  ER: number;
+  ME: number;
+  MD: number;
+  MG: number;
+  MF: number;
+  MA: number;
+  MC: number;
+  UZ: number;
+  MM: number;
+  ML: number;
+  MO: number;
+  MN: number;
+  MH: number;
+  MK: number;
+  MU: number;
+  MT: number;
+  MW: number;
+  MV: number;
+  MQ: number;
+  MP: number;
+  MS: number;
+  MR: number;
+  IM: number;
+  UG: number;
+  TZ: number;
+  MY: number;
+  MX: number;
+  IL: number;
+  FR: number;
+  IO: number;
+  SH: number;
+  FI: number;
+  FJ: number;
+  FK: number;
+  FM: number;
+  FO: number;
+  NI: number;
+  NL: number;
+  NO: number;
+  NA: number;
+  VU: number;
+  NC: number;
+  NE: number;
+  NF: number;
+  NG: number;
+  NZ: number;
+  NP: number;
+  NR: number;
+  NU: number;
+  CK: number;
+  XK: number;
+  CI: number;
+  CH: number;
+  CO: number;
+  CN: number;
+  CM: number;
+  CL: number;
+  CC: number;
+  CA: number;
+  CG: number;
+  CF: number;
+  CD: number;
+  CZ: number;
+  CY: number;
+  CX: number;
+  CR: number;
+  CW: number;
+  CV: number;
+  CU: number;
+  SZ: number;
+  SY: number;
+  SX: number;
+  KG: number;
+  KE: number;
+  SS: number;
+  SR: number;
+  KI: number;
+  KH: number;
+  KN: number;
+  KM: number;
+  ST: number;
+  SK: number;
+  KR: number;
+  SI: number;
+  KP: number;
+  KW: number;
+  SN: number;
+  SM: number;
+  SL: number;
+  SC: number;
+  KZ: number;
+  KY: number;
+  SG: number;
+  SE: number;
+  SD: number;
+  DO: number;
+  DM: number;
+  DJ: number;
+  DK: number;
+  VG: number;
+  DE: number;
+  YE: number;
+  DZ: number;
+  US: number;
+  UY: number;
+  YT: number;
+  UM: number;
+  LB: number;
+  LC: number;
+  LA: number;
+  TV: number;
+  TW: number;
+  TT: number;
+  TR: number;
+  LK: number;
+  LI: number;
+  LV: number;
+  TO: number;
+  LT: number;
+  LU: number;
+  LR: number;
+  LS: number;
+  TH: number;
+  TF: number;
+  TG: number;
+  TD: number;
+  TC: number;
+  LY: number;
+  VA: number;
+  VC: number;
+  AE: number;
+  AD: number;
+  AG: number;
+  AF: number;
+  AI: number;
+  VI: number;
+  IS: number;
+  IR: number;
+  AM: number;
+  AL: number;
+  AO: number;
+  AQ: number;
+  AS: number;
+  AR: number;
+  AU: number;
+  AT: number;
+  AW: number;
+  IN: number;
+  AX: number;
+  AZ: number;
+  IE: number;
+  ID: number;
+  UA: number;
+  QA: number;
+  MZ: number;
+}
+
+const codeToName = {
+  BD: "Bangladesh",
+  BE: "Belgium",
+  BF: "Burkina Faso",
+  BG: "Bulgaria",
+  BA: "Bosnia and Herzegovina",
+  BB: "Barbados",
+  WF: "Wallis and Futuna",
+  BL: "Saint Barthelemy",
+  BM: "Bermuda",
+  BN: "Brunei",
+  BO: "Bolivia",
+  BH: "Bahrain",
+  BI: "Burundi",
+  BJ: "Benin",
+  BT: "Bhutan",
+  JM: "Jamaica",
+  BV: "Bouvet Island",
+  BW: "Botswana",
+  WS: "Samoa",
+  BQ: "Bonaire, Saint Eustatius and Saba ",
+  BR: "Brazil",
+  BS: "Bahamas",
+  JE: "Jersey",
+  BY: "Belarus",
+  BZ: "Belize",
+  RU: "Russia",
+  RW: "Rwanda",
+  RS: "Serbia",
+  TL: "East Timor",
+  RE: "Reunion",
+  TM: "Turkmenistan",
+  TJ: "Tajikistan",
+  RO: "Romania",
+  TK: "Tokelau",
+  GW: "Guinea-Bissau",
+  GU: "Guam",
+  GT: "Guatemala",
+  GS: "South Georgia and the South Sandwich Islands",
+  GR: "Greece",
+  GQ: "Equatorial Guinea",
+  GP: "Guadeloupe",
+  JP: "Japan",
+  GY: "Guyana",
+  GG: "Guernsey",
+  GF: "French Guiana",
+  GE: "Georgia",
+  GD: "Grenada",
+  GB: "United Kingdom",
+  GA: "Gabon",
+  SV: "El Salvador",
+  GN: "Guinea",
+  GM: "Gambia",
+  GL: "Greenland",
+  GI: "Gibraltar",
+  GH: "Ghana",
+  OM: "Oman",
+  TN: "Tunisia",
+  JO: "Jordan",
+  HR: "Croatia",
+  HT: "Haiti",
+  HU: "Hungary",
+  HK: "Hong Kong",
+  HN: "Honduras",
+  HM: "Heard Island and McDonald Islands",
+  VE: "Venezuela",
+  PR: "Puerto Rico",
+  PS: "Palestinian Territory",
+  PW: "Palau",
+  PT: "Portugal",
+  SJ: "Svalbard and Jan Mayen",
+  PY: "Paraguay",
+  IQ: "Iraq",
+  PA: "Panama",
+  PF: "French Polynesia",
+  PG: "Papua New Guinea",
+  PE: "Peru",
+  PK: "Pakistan",
+  PH: "Philippines",
+  PN: "Pitcairn",
+  PL: "Poland",
+  PM: "Saint Pierre and Miquelon",
+  ZM: "Zambia",
+  EH: "Western Sahara",
+  EE: "Estonia",
+  EG: "Egypt",
+  ZA: "South Africa",
+  EC: "Ecuador",
+  IT: "Italy",
+  VN: "Vietnam",
+  SB: "Solomon Islands",
+  ET: "Ethiopia",
+  SO: "Somalia",
+  ZW: "Zimbabwe",
+  SA: "Saudi Arabia",
+  ES: "Spain",
+  ER: "Eritrea",
+  ME: "Montenegro",
+  MD: "Moldova",
+  MG: "Madagascar",
+  MF: "Saint Martin",
+  MA: "Morocco",
+  MC: "Monaco",
+  UZ: "Uzbekistan",
+  MM: "Myanmar",
+  ML: "Mali",
+  MO: "Macao",
+  MN: "Mongolia",
+  MH: "Marshall Islands",
+  MK: "Macedonia",
+  MU: "Mauritius",
+  MT: "Malta",
+  MW: "Malawi",
+  MV: "Maldives",
+  MQ: "Martinique",
+  MP: "Northern Mariana Islands",
+  MS: "Montserrat",
+  MR: "Mauritania",
+  IM: "Isle of Man",
+  UG: "Uganda",
+  TZ: "Tanzania",
+  MY: "Malaysia",
+  MX: "Mexico",
+  IL: "Israel",
+  FR: "France",
+  IO: "British Indian Ocean Territory",
+  SH: "Saint Helena",
+  FI: "Finland",
+  FJ: "Fiji",
+  FK: "Falkland Islands",
+  FM: "Micronesia",
+  FO: "Faroe Islands",
+  NI: "Nicaragua",
+  NL: "Netherlands",
+  NO: "Norway",
+  NA: "Namibia",
+  VU: "Vanuatu",
+  NC: "New Caledonia",
+  NE: "Niger",
+  NF: "Norfolk Island",
+  NG: "Nigeria",
+  NZ: "New Zealand",
+  NP: "Nepal",
+  NR: "Nauru",
+  NU: "Niue",
+  CK: "Cook Islands",
+  XK: "Kosovo",
+  CI: "Ivory Coast",
+  CH: "Switzerland",
+  CO: "Colombia",
+  CN: "China",
+  CM: "Cameroon",
+  CL: "Chile",
+  CC: "Cocos Islands",
+  CA: "Canada",
+  CG: "Republic of the Congo",
+  CF: "Central African Republic",
+  CD: "Democratic Republic of the Congo",
+  CZ: "Czech Republic",
+  CY: "Cyprus",
+  CX: "Christmas Island",
+  CR: "Costa Rica",
+  CW: "Curacao",
+  CV: "Cape Verde",
+  CU: "Cuba",
+  SZ: "Swaziland",
+  SY: "Syria",
+  SX: "Sint Maarten",
+  KG: "Kyrgyzstan",
+  KE: "Kenya",
+  SS: "South Sudan",
+  SR: "Suriname",
+  KI: "Kiribati",
+  KH: "Cambodia",
+  KN: "Saint Kitts and Nevis",
+  KM: "Comoros",
+  ST: "Sao Tome and Principe",
+  SK: "Slovakia",
+  KR: "South Korea",
+  SI: "Slovenia",
+  KP: "North Korea",
+  KW: "Kuwait",
+  SN: "Senegal",
+  SM: "San Marino",
+  SL: "Sierra Leone",
+  SC: "Seychelles",
+  KZ: "Kazakhstan",
+  KY: "Cayman Islands",
+  SG: "Singapore",
+  SE: "Sweden",
+  SD: "Sudan",
+  DO: "Dominican Republic",
+  DM: "Dominica",
+  DJ: "Djibouti",
+  DK: "Denmark",
+  VG: "British Virgin Islands",
+  DE: "Germany",
+  YE: "Yemen",
+  DZ: "Algeria",
+  US: "United States",
+  UY: "Uruguay",
+  YT: "Mayotte",
+  UM: "United States Minor Outlying Islands",
+  LB: "Lebanon",
+  LC: "Saint Lucia",
+  LA: "Laos",
+  TV: "Tuvalu",
+  TW: "Taiwan",
+  TT: "Trinidad and Tobago",
+  TR: "Turkey",
+  LK: "Sri Lanka",
+  LI: "Liechtenstein",
+  LV: "Latvia",
+  TO: "Tonga",
+  LT: "Lithuania",
+  LU: "Luxembourg",
+  LR: "Liberia",
+  LS: "Lesotho",
+  TH: "Thailand",
+  TF: "French Southern Territories",
+  TG: "Togo",
+  TD: "Chad",
+  TC: "Turks and Caicos Islands",
+  LY: "Libya",
+  VA: "Vatican",
+  VC: "Saint Vincent and the Grenadines",
+  AE: "United Arab Emirates",
+  AD: "Andorra",
+  AG: "Antigua and Barbuda",
+  AF: "Afghanistan",
+  AI: "Anguilla",
+  VI: "U.S. Virgin Islands",
+  IS: "Iceland",
+  IR: "Iran",
+  AM: "Armenia",
+  AL: "Albania",
+  AO: "Angola",
+  AQ: "Antarctica",
+  AS: "American Samoa",
+  AR: "Argentina",
+  AU: "Australia",
+  AT: "Austria",
+  AW: "Aruba",
+  IN: "India",
+  AX: "Aland Islands",
+  AZ: "Azerbaijan",
+  IE: "Ireland",
+  ID: "Indonesia",
+  UA: "Ukraine",
+  QA: "Qatar",
+  MZ: "Mozambique",
+};
