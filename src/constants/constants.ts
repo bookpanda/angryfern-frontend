3bf9eecca6e8ea32bb2ba72eb04721000db38f17
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-17 14:52:21 +0700

    feat: speed env

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
 export const API_URL = process.env.API_URL;
 export const API_KEY = process.env.API_KEY;
-export const SPEED_THRESHOLD = 5;
+export const SPEED_THRESHOLD = Number(process.env.SPEED_THRESHOLD ?? "5");
