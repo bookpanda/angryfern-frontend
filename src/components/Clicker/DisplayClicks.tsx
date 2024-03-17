4d5cf756a9f51afe8c220d1100007ce6ee055ab2
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-17 13:55:44 +0700

    fix: add more z

--- src/components/Clicker/DisplayClicks.tsx
+++ src/components/Clicker/DisplayClicks.tsx
@@ -7,7 +7,7 @@ import { selectCount } from "./clickerSlice";
 export const DisplayClicks = () => {
   const count = useAppSelector((state) => selectCount(state));
   return (
-    <div className="fixed z-10 flex w-full flex-col items-center pt-4">
+    <div className="fixed z-20 flex w-full flex-col items-center pt-4">
       <Text variant="h1" className="text-white">
         ANGRYFERN
       </Text>
--- src/components/Scoreboard/Scoreboard.tsx
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -16,7 +16,7 @@ export const Scoreboard = () => {
       <Button
         variant="secondary"
         className={clsx(
-          "absolute right-4 top-[15%] w-16",
+          "absolute right-4 top-[15%] z-20 w-16",
           !show && "opacity-30"
         )}
         onClick={handleClick}
@@ -25,7 +25,7 @@ export const Scoreboard = () => {
       </Button>
       <div
         className={clsx(
-          "absolute right-4 top-[20%] h-1/2 w-1/6 overflow-scroll rounded-lg duration-300 ease-in-out",
+          "absolute right-4 top-[20%] z-20 h-1/2 w-1/6 overflow-scroll rounded-lg duration-300 ease-in-out",
           !show && "opacity-0"
         )}
       >
